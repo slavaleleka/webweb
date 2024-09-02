cc10c395eadaa446008821e240668a284779a981
Slava Leleka <v.leleka@adguard.com>
2024-09-02 16:28:06 +0300

    one more update

--- testing/pages/png-redirect-script.js
+++ testing/pages/png-redirect-script.js
@@ -2,8 +2,8 @@ function loadUnsafeResource() {
     // Attempt to load a resource that redirects to an unsafe domain
     const img = document.createElement('img');
 
-    img.src = 'https://slavaleleka.github.io/webweb/testing/pages/unsafe-redirect-png.png';
-    // img.src = 'https://raw.githubusercontent.com/slavaleleka/webweb/master/testing/pages/unsafe-redirect-png.png';
+    img.src = 'https://slavaleleka.github.io/webweb/testing/pages/png-redirect.png';
+    // img.src = 'https://raw.githubusercontent.com/slavaleleka/webweb/master/testing/pages/png-redirect.png';
 
     img.onload = () => console.log('Image loaded successfully');
     img.onerror = (err) => console.error('Failed to load image:', err);
