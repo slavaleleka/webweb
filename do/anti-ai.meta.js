688f6e2b17d3b0fe993519d585cddc1114717c6f
Slava Leleka <v.leleka@adguard.com>
2025-06-28 00:34:07 -0400

    one more update

---
+++ do/anti-ai.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Anti-AI Elements
+// @namespace    http://tampermonkey.net/
+// @version      0.1.5
+// @description  Hides Google Search AI Overview.
+// @author       Slava Leleka @slavaleleka
+// @license      MIT
+// @downloadURL  https://slavaleleka.github.io/webweb/do/anti-ai.user.js
+// @updateURL    https://slavaleleka.github.io/webweb/do/anti-ai.meta.js
+// @match        *://www.google.com/*
+// @match        *://www.google.co.uk/*
+// @match        *://www.google.ca/*
+// @match        *://www.google.com.au/*
+// @match        *://www.google.de/*
+// @match        *://www.google.fr/*
+// @match        *://www.google.*/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
--- do/anti-ai.user.js
+++ do/anti-ai.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Anti-AI Elements
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  Hides Google Search AI Overview.
 // @author       Slava Leleka @slavaleleka
 // @license      MIT
 // @downloadURL  https://slavaleleka.github.io/webweb/do/anti-ai.user.js
+// @updateURL    https://slavaleleka.github.io/webweb/do/anti-ai.meta.js
 // @match        *://www.google.com/*
 // @match        *://www.google.co.uk/*
 // @match        *://www.google.ca/*
